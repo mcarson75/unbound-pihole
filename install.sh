0a97b21211f4c6ab6d57cd8aabd457d3d1d6d850
mcarson75 <mcarson75@gmail.com>
2019-09-18 18:34:15 +0545

    Update to Pi-Hole v4.3.2

--- install.sh
+++ install.sh
@@ -24,6 +24,7 @@ mv "$(which debconf-apt-progress)" /bin/no_debconf-apt-progress
 
 # Get the install functions
 curl https://raw.githubusercontent.com/pi-hole/pi-hole/${CORE_VERSION}/automated%20install/basic-install.sh > "$PIHOLE_INSTALL"
+chmod +x "$PIHOLE_INSTALL"
 PH_TEST=true . "${PIHOLE_INSTALL}"
 
 # Preseed variables to assist with using --unattended install
@@ -47,19 +48,21 @@ distro_check
 apt-get -y install debconf-utils
 echo resolvconf resolvconf/linkify-resolvconf boolean false | debconf-set-selections
 
-# Tried this - unattended causes starting services during a build, should probably PR a flag to shut that off and switch to that 
-#bash -ex "./${PIHOLE_INSTALL}" --unattended
-install_dependent_packages INSTALLER_DEPS[@]
-install_dependent_packages PIHOLE_DEPS[@]
-install_dependent_packages PIHOLE_WEB_DEPS[@]
+ln -s /bin/true /usr/local/bin/service
+bash -ex "${PIHOLE_INSTALL}" --unattended
+rm /usr/local/bin/service
+# Old way of setting up
+#install_dependent_packages INSTALLER_DEPS[@]
+#install_dependent_packages PIHOLE_DEPS[@]
+#install_dependent_packages PIHOLE_WEB_DEPS[@]
 # IPv6 support for nc openbsd better than traditional
 apt-get install -y --force-yes netcat-openbsd
 
 piholeGitUrl="${piholeGitUrl}"
 webInterfaceGitUrl="${webInterfaceGitUrl}"
 webInterfaceDir="${webInterfaceDir}"
-git clone --branch "${CORE_VERSION}" --depth 1 "${piholeGitUrl}" "${PI_HOLE_LOCAL_REPO}"
-git clone --branch "${WEB_VERSION}" --depth 1 "${webInterfaceGitUrl}" "${webInterfaceDir}"
+#git clone --branch "${CORE_VERSION}" --depth 1 "${piholeGitUrl}" "${PI_HOLE_LOCAL_REPO}"
+#git clone --branch "${WEB_VERSION}" --depth 1 "${webInterfaceGitUrl}" "${webInterfaceDir}"
 
 tmpLog="/tmp/pihole-install.log"
 installLogLoc="${installLogLoc}"
